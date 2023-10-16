e5ff0403bc902f6101fe705bcc1509f592864503
hendynursholeh <hendynursholeh7213@gmail.com>
2023-10-16 23:24:30 +0800

    add migration : users

---
+++ app/Database/Migrations/2023-10-16-143636_User.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class User extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'VARCHAR',
+                'constraint' => 50,
+            ],
+            'username' => [
+                'type' => 'VARCHAR',
+                'constraint' => 100,
+            ],
+            'password' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'email' => [
+                'type' => 'VARCHAR',
+                'constraint' => 100,
+            ],
+            'name' => [
+                'type' => 'VARCHAR',
+                'constraint' => 100,
+            ],
+            'phone' => [
+                'type' => 'VARCHAR',
+                'constraint' => 15,
+            ],
+            'address' => [
+                'type' => 'TEXT',
+            ],
+            'role' => [
+                'type' => "ENUM('super admin', 'eo', 'user')",
+                'default' => 'user'
+            ]
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('users');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('users');
+    }
+}
